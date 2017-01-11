a1fbc68160a14e25d33518418df844ed546ef646
liuyang <yang.liu@mi-me.com>
2017-01-11 16:29:18 +0800

    Initial Commit

---
+++ LYGoodsDetailDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYGoodsDetailDemo
+//
+//  Created by liuyang on 17/1/11.
+//  Copyright © 2017年 Mime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
